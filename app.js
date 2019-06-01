6e14eb92c2dc8df8a1c6c0c9aea25d8917d0ac04
enriqueromi <kikileno.79716@gmail.com>
2019-06-01 22:21:39 +0200

    .

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var app = express();
 
 
 // Cors
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+//     next();
+// });
 
-
+app.use(allowCrossDomain)
 
 // Body Parser
 // parse application/x-www-form-urlencoded
