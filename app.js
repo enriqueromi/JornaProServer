2d6e42e73058bec175f20bac1942f31dbdad0875
enriqueromi <kikileno.79716@gmail.com>
2019-06-04 19:34:19 +0200

    .

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Request-Method', '*');
     res.header('Access-Control-Max-Age', '1728000');
-
+    next();
 });
 
 
