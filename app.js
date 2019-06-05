673ebd1f3fddb170a842c91da53bc0dcb9b2743b
enriqueromi <kikileno.79716@gmail.com>
2019-06-05 20:29:18 +0200

    .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('./config/config');
 
 
 // Inicializar variables
-var app = express();
+const app = express();
 
 
 // Cors
@@ -17,6 +17,22 @@ var app = express();
 //     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 //     next();
 // });
+// Create express server
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
 
 
