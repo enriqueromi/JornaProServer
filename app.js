536fc596e9d468e0ead32959ee257717b4b1b206
enriqueromi <kikileno.79716@gmail.com>
2019-06-05 20:51:44 +0200

    .

--- app.js
+++ app.js
@@ -18,29 +18,16 @@ const app = express();
 //     next();
 // });
 // Create express server
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://jornaproserve.herokuapp.com/');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
+app.use(cors());
+app.options('*', cors());
 
 
 
 // Body Parser
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 
 
